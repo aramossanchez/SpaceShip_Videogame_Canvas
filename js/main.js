479b011a42ce501f40dceed86df0acf139bfb497
Armando <armandoramossanchez@gmail.com>
2021-10-16 15:05:28 +0200

    preparo entorno

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// AUTOR: ARMANDO RAMOS SÁNCHEZ
+// VERSION: 1.0
+
+//ENTORNO
+
+//REFERENCIO CANVAS Y CREO CONTEXTO 2D
+let canvas = document.getElementById("game");
+let contexto = canvas.getContext("2d");
+
