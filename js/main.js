470d71b33eb2642fa840ee297615fae3e7b7c1c3
Armando <armandoramossanchez@gmail.com>
2021-10-16 15:12:23 +0200

    creacion del coche

--- js/main.js
+++ js/main.js
@@ -7,3 +7,28 @@
 let canvas = document.getElementById("game");
 let contexto = canvas.getContext("2d");
 
+//CREO VARIABLES PARA COCHE
+var cocheWidth = 60;
+var cocheHeight = 35;
+var cochePosicionX = 0;
+var cochePosicionY = canvas.height-cocheHeight;
+
+//CREO COCHE
+const dibujarCoche = () =>{
+    contexto.beginPath();
+    contexto.rect(0, canvas.height-cocheHeight, cocheWidth, cocheHeight);
+    contexto.fillStyle = "#0095DD";
+    contexto.fill();
+    contexto.closePath();
+}
+//CREAMOS VARIABLES PARA EL MOVIMIENTO DEL COCHE
+var cocheX = canvas.width/2;
+var cocheY = canvas.height-30;
+
+var movimientoX = 2;
+var movimientoY = -2;
+
+dibujarCoche();
+
+
+
