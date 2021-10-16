486626f3c7e594b5c8c8c0eb74514544cb97c770
Armando <armandoramossanchez@gmail.com>
2021-10-16 19:23:14 +0200

    movimiento corregido hacia todos los lados

--- js/main.js
+++ js/main.js
@@ -7,15 +7,29 @@
 let canvas = document.getElementById("game");
 let contexto = canvas.getContext("2d");
 
-//CREO VARIABLES PARA COCHE
+//CREO VARIABLES PARA LA NAVE
 var naveWidth = 35;
 var naveHeight = 35;
 var navePosicionX = 0;
 var navePosicionY = canvas.height-naveHeight;
 
+//CREAMOS VARIABLES PARA LOS BORDES
+var bordeIzquierdaWidth = 15;
+var bordeIzquierdaHeight = canvas.height;
+
+var bordeDerechaWidth = 15;
+var bordeDerechaHeight = canvas.height;
+
+var bordeArribaWidth = canvas.width;
+var bordeArribaHeight = 15;
+
+var bordeAbajoWidth = canvas.width;
+var bordeAbajoHeight = 15;
+
+
 //CREAMOS VARIABLES PARA EL MOVIMIENTO DEL nave
-var naveX = 0;
-var naveY = canvas.height-naveHeight;
+var naveX = canvas.width/2 - naveWidth;
+var naveY = canvas.height/2 - naveHeight;
 
 var velocidadX = 0;
 var velocidadY = 0;
@@ -34,11 +48,36 @@ var haciaAbajo = false;
 const dibujarnave = () =>{
     contexto.beginPath();
     contexto.rect(naveX, naveY, naveWidth, naveHeight);
-    contexto.fillStyle = "#0095DD";
+    contexto.fillStyle = "#FF0000";
     contexto.fill();
     contexto.closePath();
 }
 
+const pintarBordes = () =>{
+    contexto.beginPath();
+    contexto.rect(0, 0, bordeIzquierdaWidth, bordeIzquierdaHeight);
+    contexto.fillStyle = "#00FF00";
+    contexto.fill();
+    contexto.closePath();
+
+    contexto.beginPath();
+    contexto.rect(0, 0, bordeArribaWidth, bordeArribaHeight);
+    contexto.fillStyle = "#00FF00";
+    contexto.fill();
+    contexto.closePath();
+
+    contexto.beginPath();
+    contexto.rect(canvas.width - bordeDerechaWidth, 0, bordeDerechaWidth, bordeDerechaHeight);
+    contexto.fillStyle = "#00FF00";
+    contexto.fill();
+    contexto.closePath();
+
+    contexto.beginPath();
+    contexto.rect(0, canvas.height - bordeAbajoHeight, bordeAbajoWidth, bordeAbajoHeight);
+    contexto.fillStyle = "#00FF00";
+    contexto.fill();
+    contexto.closePath();
+}
 //CREAMOS EVENTOS PARA PULSACIÓN DE TECLAS Y MOVIMIENTO DE STICK
 const pulsarTecla = (e) =>{
     if(e.keyCode == 37) {
@@ -77,72 +116,80 @@ document.addEventListener("keyup", levantarTecla, false);
 const juego = () =>{
     //SE BORRA TODO EL CONTENIDO DEL CANVAS DENTRO DE SUS MARGENES. COORDENADAS X E Y DE ESQUINA SUPERIRO IZQUIERDA Y COORDENADAS X E Y DE ESQUINA INFERIOR DERECHA DE UN RECTANGULO
     contexto.clearRect(0, 0, canvas.width, canvas.height);
-    //PINTAMOS nave
+    //PINTAMOS NAVE
     dibujarnave();
+    //PINTAMOS BORDES
+    pintarBordes();
     //GESTIONAMOS MOVIMIENTO DE LA NAVE
     
     //MOVIMIENTO HACIA LA IZQUIERDA
     if(izquierdaPulsado){
         haciaIzquierda = true;
         haciaDerecha = false;
-        if (velocidadX > -8){
+        if (velocidadX > -5){
             velocidadX -= 0.10;
         };
         naveX += velocidadX;
     };
-    if(!izquierdaPulsado && haciaIzquierda && naveX > 0){
+    if(!izquierdaPulsado){
         naveX += velocidadX;
     };
-    if(naveX <= 0){
-        naveX = 0;
+    if(naveX <= 0 + bordeIzquierdaWidth){
+        naveX = 0 + bordeIzquierdaWidth;
         velocidadX = 0;
     }
     //MOVIMIENTO HACIA ARRIBA
     if(arribaPulsado){
         haciaArriba = true;
         haciaAbajo = false;
-        if (velocidadY > -8){
+        haciaIzquierda = false;
+        haciaDerecha = false;
+        if (velocidadY > -5){
             velocidadY -= 0.10;
         };
         naveY += velocidadY;
     };
-    if(!arribaPulsado && haciaArriba && naveY > 0){
+    if(!arribaPulsado){
         naveY += velocidadY;
     };
-    if(naveY <= 0){
-        naveY = 0;
+    if(naveY <= 0 + bordeArribaHeight){
+        naveY = 0 + bordeArribaHeight;
         velocidadY = 0;
     }
     //MOVIMIENTO HACIA DERECHA
-    if(derechaPulsado && naveX < canvas.width - naveWidth){
-        haciaDerecha = true;
+    if(derechaPulsado){
+        haciaArriba = false;
+        haciaAbajo = false;
         haciaIzquierda = false;
-        if (velocidadX < 8){
+        haciaDerecha = true;
+        if (velocidadX < 5){
             velocidadX += 0.10;
         }
         naveX += velocidadX;
     };
-    if(!derechaPulsado && haciaDerecha && naveX < canvas.width - naveWidth){
+    if(!derechaPulsado){
         naveX += velocidadX;
     };
-    if(naveX >= canvas.width - naveWidth){
-        naveX = canvas.width - naveWidth;
+    if(naveX >= canvas.width - naveWidth - bordeDerechaWidth){
+        naveX = canvas.width - naveWidth - bordeDerechaWidth;
         velocidadX = 0;
     }
     //MOVIMIENTO HACIA ARRIBA
     if(abajoPulsado){
-        haciaAbajo = true;
         haciaArriba = false;
-        if (velocidadY < 8){
+        haciaAbajo = true;
+        haciaIzquierda = false;
+        haciaDerecha = false;
+        if (velocidadY < 5){
             velocidadY += 0.10;
         };
         naveY += velocidadY;
     };
-    if(!abajoPulsado && haciaAbajo && naveY > 0){
+    if(!abajoPulsado){
         naveY += velocidadY;
     };
-    if(naveY <= 0){
-        naveY = 0;
+    if(naveY >= canvas.height-naveHeight-bordeAbajoHeight){
+        naveY = canvas.height-naveHeight-bordeAbajoHeight;
         velocidadY = 0;
     }
     //CUANDO LLAMEMOS A LA FUNCIÓN, SE EJECUTARÁ EN BUCLE CON LA TASA DE REFRESCOS MÁXIMA QUE SOPORTE EL NAVEGADOR
